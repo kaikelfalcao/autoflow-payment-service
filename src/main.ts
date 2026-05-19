4874965a6a4817609adfd2b1e8367ce36bbd5a49
Kaike Falcão <kaikelfalcao@gmail.com>
2026-05-19 00:46:17 -0300

    refactor(main): normalizar bootstrap entre serviços
    - PORT é a env padronizada (APP_PORT fica como fallback em order/saga)
    - Swagger sempre em /api/docs (era 'docs' em order/payment)
    - ValidationPipe consistente: { whitelist, transform, forbidNonWhitelisted }
    - Remover writeFileSync('swagger.json') que poluía o working tree

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ if (process.env.NEW_RELIC_ENABLED === "true") {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   require("newrelic");
 }
-import { writeFileSync } from "fs";
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe, Logger } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
@@ -10,13 +9,17 @@ import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
 import { AppModule } from "./app.module";
 import { HttpExceptionFilter } from "./shared/filters/http-exception.filter";
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   const logger = new Logger("Bootstrap");
 
   app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
 
@@ -27,9 +30,7 @@ async function bootstrap() {
     .build();
 
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup("docs", app, swaggerDocument);
-
-  writeFileSync("swagger.json", JSON.stringify(swaggerDocument, null, 2));
+  SwaggerModule.setup("api/docs", app, swaggerDocument);
 
   const port = Number(process.env.PORT ?? 3004);
   await app.listen(port);
